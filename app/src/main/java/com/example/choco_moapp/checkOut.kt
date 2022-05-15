6beaa0fa51087f59d742e05bebb636aa139f99c0
nadulatj <8mark8rex@gmail.com>
2022-05-15 22:18:06 +0530

    Checkout page

--- app/src/main/java/com/example/choco_moapp/checkOut.kt
+++ app/src/main/java/com/example/choco_moapp/checkOut.kt
@@ -2,13 +2,36 @@ package com.example.choco_moapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.ShapeAppearanceModel
 
 class checkOut : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R layout.activity_check_out)
-        android:layout_width="match_parent"
-        android:layout_height="match_parent"
-        tools:context=".checkOut">
+        val search= findViewById<TextView>(R.id.main_search)
+        val shapeable= findViewById<ShapeableImageView>(R.id.main_shapeable)
+        val recyclerView= findViewById<RecyclerView>(R.id.main_recyclerView)
+
+        shapeable.shapeAppearanceModel= ShapeAppearanceModel().toBuilder().setAllCorners(
+            CornerFamily.ROUNDED, 20F).build()
+
+        val itemList= ArrayList<Category>()
+        val adapter= CategoryAdapter(itemList)
+
+        setupRecyclerView(adapter,recyclerView)
+
+        val db= FirebaseDatabase.getInstance().reference
+        val valueEventListener= object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for(i in snapshot.children){
+                    itemList.add(Category(i.key.toString(), i.child("image").getValue().toString()))
+                }
+                adapter.notifyDataSetChanged()
+            }
+
     }
 }
