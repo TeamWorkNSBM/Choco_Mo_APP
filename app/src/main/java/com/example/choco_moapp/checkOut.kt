f3835897c7472bb86a248e7613a3d6bd979b2be8
nadulatj <8mark8rex@gmail.com>
2022-05-15 22:09:44 +0530

    Checkout page

---
+++ app/src/main/java/com/example/choco_moapp/checkOut.kt
@@ -0,0 +1,14 @@
+package com.example.choco_moapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class checkOut : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R layout.activity_check_out)
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        tools:context=".checkOut">
+    }
+}
